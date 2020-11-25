874372aeb81938b4c668e5ed9983ce78a5551add
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2020-11-25 10:44:35 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-
-    // Base URLS
-    app.use('/', require('./base.routes.js'))
-    app.use('/coasters', require('./coaster.routes'))
-    app.use('/parks', require('./park.routes'))
-}
